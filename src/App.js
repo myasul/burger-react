f9cabe0a3e506dc564d868e1294c6d7e3a9335cd
myasul <yasulmatthew@gmail.com>
2018-07-16 16:16:22 +0800

    Create Burger Builder template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuider';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Layout>
-                    Test
+                    <BurgerBuilder />
                 </Layout>
             </div>
         );
