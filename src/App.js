8e5925a88d2ea3eb025241123b13eba36f007de7
myasul <yasulmatthew@gmail.com>
2018-07-16 16:02:17 +0800

    Add initial layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import Layout from './components/Layout/Layout'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Layout>
+                    Test
+                </Layout>
             </div>
         );
     }
