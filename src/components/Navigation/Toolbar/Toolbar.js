14c97e99bfd6c3c6e937c2ed6394f27e9918151b
Matthew Yasul <yasulmatthew@gmail.com>
2018-07-19 10:37:46 +0800

    Implement NavigationItems/Item component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItem from '../NavigationItems/NavigationItems';
 
 
 const toolbar = () => (
     <div className={classes.Toolbar}>
         <div>MENU</div>
         <Logo />
-        <nav>...</nav>
+        <nav>
+            <NavigationItem />
+        </nav>
     </div>
 );
 
