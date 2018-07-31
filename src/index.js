0d36d420b72595ec307130ebf267b88095593976
Matthew Yasul <yasulmatthew@gmail.com>
2018-07-31 08:10:19 +0800

    Set up Redux devtools

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer, devToolsEnhancer());
 
 const app = (
     <Provider store={store}>
