517d04ad4f192967004d7d400981b2c17a611f1c
Matthew Yasul <yasulmatthew@gmail.com>
2018-08-04 20:28:58 +0800

    Load Orders, Checkout and Auth containers lazily

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import { connect } from 'react-redux';
 
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 import classes from './App.css';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const asyncCheckout = asyncComponent(() => {
+    return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+    return import('./containers/Orders/Orders');
+})
+
+const asyncAuth = asyncComponent(() => {
+    return import('./containers/Auth/Auth');
+})
 
 class App extends Component {
 
@@ -21,10 +31,10 @@ class App extends Component {
         if (this.props.isAuthenticated) {
             routes = (
                 <Switch>
-                    <Route path='/checkout' component={Checkout} />
-                    <Route path='/orders' component={Orders} />
+                    <Route path='/checkout' component={asyncCheckout} />
+                    <Route path='/orders' component={asyncOrders} />
                     <Route path='/logout' component={Logout} />
-                    <Route path='/auth' component={Auth} />
+                    <Route path='/auth' component={asyncAuth} />
                     <Route path='/' exact component={BurgerBuilder} />
                     <Redirect to='/' />
                 </Switch>
@@ -32,7 +42,7 @@ class App extends Component {
         } else {
             routes = (
                 <Switch>
-                    <Route path='/auth' component={Auth} />
+                    <Route path='/auth' component={asyncAuth} />
                     <Route path='/' exact component={BurgerBuilder} />
                     <Redirect to='/' />
                 </Switch>
